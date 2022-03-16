c9fc45c12537ec4ab6445fbef4e4e9a94c53cdbe
Wilson Kao <97723790+wilsonfish@users.noreply.github.com>
2022-03-17 00:08:51 +0800

    Delete initial directory

--- initial/src/main/java/com/example/springboot/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.springboot;
-
-import java.util.List;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product,Long> {
-}
